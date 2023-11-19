a711b636cbe10c434c2b1a0d30c6e39f4af15484
mohammadmurtada <moha135h@stud.kea.dk>
2023-11-19 05:27:40 +0100

    Ændring af mail

--- src/main/java/com/monero/multibooks/MultiBooks/Service/Auth/AuthService.java
+++ src/main/java/com/monero/multibooks/MultiBooks/Service/Auth/AuthService.java
@@ -66,7 +66,7 @@ public class AuthService {
         try {
             Message message = new MimeMessage(session);
 
-            message.setFrom(new InternetAddress("your-email@example.com"));
+            message.setFrom(new InternetAddress("keamohammadmnodejs@gmail.com"));
 
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
 
