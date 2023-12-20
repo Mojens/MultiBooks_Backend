4baa3cd4f1562d70a67ac4c349fe9bd82b09eeda
mohammadmurtada <moha135h@stud.kea.dk>
2023-12-20 19:47:21 +0100

    Fix deployment

--- src/test/java/com/monero/multibooks/MultiBooksApplicationTests.java
+++ src/test/java/com/monero/multibooks/MultiBooksApplicationTests.java
@@ -3,7 +3,7 @@ package com.monero.multibooks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MultiBooksApplicationTests {
 
     @Test
